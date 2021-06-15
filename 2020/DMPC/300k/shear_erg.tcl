2f64755ad5bfe840f56ac7d264a28417fffcc4d2
Fer Favela <ilhan.eins@gmail.com>
2021-06-15 10:52:08 -0500

    .

---
+++ 2020/DMPC/300k/shear_erg.tcl
@@ -0,0 +1,56 @@
+# VMD for MACOSXX86_64, version 1.9.4a42 (May 9, 2020)
+# Log file '/Users/ffavela/Documents/GitHub/ICF/2020/Ternaries/GRO_FILES/shear.vmd', created by user ffavela
+display projection Orthographic
+menu graphics off
+menu graphics on
+menu graphics off
+menu color off
+menu color on
+color Display Background white
+menu color off
+menu display off
+menu display on
+display shadows on
+display ambientocclusion on
+display dof on
+menu display off
+axes location Off
+display depthcue off
+display rendermode Acrobat3D
+menu graphics off
+menu graphics on
+mol modselect 0 0 {resname DMPC and name "C2.*" and not name C2}
+mol modmaterial 0 0 AOChalky
+mol modstyle 0 0 VDW 1.000000 12.000000
+mol modstyle 0 0 VDW 0.900000 12.000000
+mol modstyle 0 0 VDW 0.800000 12.000000
+mol modcolor 0 0 ColorID 0
+mol modcolor 0 0 ColorID 8
+mol color ColorID 8
+mol representation VDW 0.800000 12.000000
+mol selection {resname DMPC and name "C2.*" and not name C2}
+mol material AOChalky
+mol addrep 0
+mol modselect 1 0 {resname DMPC and name "C3.*" and not name C2}
+mol color ColorID 8
+mol representation VDW 0.800000 12.000000
+mol selection {resname DMPC and name "C2.*" and not name C2}
+mol material AOChalky
+mol addrep 0
+mol modselect 2 0 {resname SM16 and name "C2.*" and not name C2}
+mol modcolor 2 0 ColorID 7
+mol color ColorID 7
+mol representation VDW 0.800000 12.000000
+mol selection {resname SM16 and name "C2.*" and not name C2}
+mol material AOChalky
+mol addrep 0
+mol modselect 3 0 {resname SM16 and name "C3.*" and not name C2}
+mol color ColorID 7
+mol representation VDW 0.800000 12.000000
+mol selection {resname SM16 and name "C2.*" and not name C2}
+mol material AOChalky
+mol addrep 0
+mol modselect 4 0 {resname ERG and not hydrogen}
+mol modcolor 4 0 ColorID 10
+# VMD for MACOSXX86_64, version 1.9.4a42 (May 9, 2020)
+# end of log file.
